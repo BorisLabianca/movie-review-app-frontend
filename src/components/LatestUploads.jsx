99aab5cd653356874848dcb3c3287e6b62c4cc2b
Boris Labianca <boris.labianca@hotmail.fr>
2023-07-06 21:54:06 -0700

    added latest movies to dashboard

--- src/components/LatestUploads.jsx
+++ src/components/LatestUploads.jsx
@@ -1,21 +1,37 @@
-import { BsBoxArrowUpRight, BsPencilSquare, BsTrash } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import { getMovies } from "../api/movie";
+import { useNotification } from "../hooks";
 import MovieListItem from "./MovieListItem";
 
+const pageNumber = 0;
+const limit = 5;
+
 const LatestUploads = () => {
+  const { updateNotification } = useNotification();
+  const [latestMovies, setLatestMovies] = useState([]);
+  const fetchLatestUploads = async () => {
+    const { error, movies } = await getMovies(pageNumber, limit);
+
+    if (error) return updateNotification("error", error);
+
+    setLatestMovies([...movies]);
+  };
+
+  useEffect(() => {
+    fetchLatestUploads();
+  }, []);
   return (
     <div className="bg-white shadow-md shadow-300y-500 dark:shadow-gray-700 dark:bg-secondary p-5 rounded col-span-2">
       <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
         Recent Uploads
       </h1>
-      <MovieListItem
-        movie={{
-          poster:
-            "https://images.unsplash.com/photo-1680817976135-91487402512f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=80",
-          title: "Lorem ipsum dolor sit amet.",
-          status: "Public",
-          genres: ["Action", "Comedy", "Adventure"],
-        }}
-      />
+      <div className="space-y-3">
+        {latestMovies
+          ? latestMovies.map((latestMovie) => {
+              return <MovieListItem movie={latestMovie} key={latestMovie.id} />;
+            })
+          : null}
+      </div>
     </div>
   );
 };
